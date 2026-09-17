79a4f812c8d048821949ab07e55a8ee350cf67cf
kyywebdep <sulhan.rijki.fict@krw.horizon.ac.id>
2026-09-17 10:10:22 +0700

    Praktikum Tipe Data Python

---
+++ praktikum-tipe-data/activity3_rijki.py/activity3_rijki.py
@@ -0,0 +1,48 @@
+nama = "Sulhan Muhammad Rijki"
+umur = 20
+ipk = 3.95
+mahasiswa_aktif = True
+
+print(nama)
+print(umur)
+print(ipk)
+print(mahasiswa_aktif)
+
+print(type(nama))
+print(type(umur))
+print(type(ipk))
+print(type(mahasiswa_aktif))
+
+print(len(nama))
+
+
+# Percobaan A - Float -> Integer
+
+nilai = 87.75
+
+print(nilai)
+print(type(nilai))
+
+nilai_baru = int(nilai)
+
+print(nilai_baru)
+print(type(nilai_baru))
+
+
+# Percobaan B - Integer -> String
+
+skor = 90
+skor_teks = str(skor)
+
+print("Nilai saya adalah " + skor_teks)
+print(type(skor_teks))
+
+
+# Percobaan C - String -> Float
+
+berat = "65.5"
+berat_baru = float(berat)
+
+print(berat_baru)
+print(type(berat_baru))
+print(berat_baru + 2.5)
